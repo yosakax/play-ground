7210ae8b2b9ad7c34555c8f047376362eaa9dc49
yosaka <fallingfluit@gmail.com>
2024-08-17 01:15:43 +0900

    random-greeting

--- golang/greetings/greetings.go
+++ golang/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // hello returns a greting for the named person
@@ -12,6 +13,16 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
